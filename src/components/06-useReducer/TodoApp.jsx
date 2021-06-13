b7489d34dd1f258ec1980e3a4419621189495d9f
Hozanno Raphael <hozanno11@gmail.com>
2021-06-13 09:32:40 +0200

    Optimización

--- src/components/06-useReducer/TodoApp.jsx
+++ src/components/06-useReducer/TodoApp.jsx
@@ -1,113 +1,67 @@
-import React from 'react'
-import { useReducer } from 'react';
-import { todoReducer } from './todoReducer';
-import "./style.css"
-import { useForm } from '../../hooks/useForm';
-import { useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
+import './style.css';
+import { todoReducer } from './todoReducer';
 
 const init = () => {
-    return JSON.parse(localStorage.getItem("task")) || []; /* [
-        {
-            id: new Date().getTime(),
-            desc: "Aprender React",
-            done: false
-        }
-    ]; */
+
+    return JSON.parse(localStorage.getItem('todos')) || [];
+
 }
+
 export const TodoApp = () => {
 
     const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-    const [{ desc }, handleOnChange, reset] = useForm({
-        desc: ""
-    });
-
     useEffect(() => {
-        localStorage.setItem("task", JSON.stringify(todos));
+        localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
 
-        if (desc.trim().length <= 0) return;
-        const newTodo = {
-            id: new Date().getTime(),
-            desc,
-            done: false
+    const handleDeleteTask = (todoId) => {
+
+        const action = {
+            type: 'delete',
+            payload: todoId
         }
 
-        const addActionTodo = {
-            type: "add",
-            payload: newTodo
-        }
-
-        dispatch(addActionTodo);
-        // limpiar el formulario
-        reset();
+        dispatch(action);
     }
 
-    const handleToogle = (todoId) => {
+    const handleToggle = (todoId) => {
 
         dispatch({
-            type: "toogle",
+            type: 'toggle',
             payload: todoId
+        });
+
+    }
+
+    const handleAddTodo = (newTodo) => {
+
+        dispatch({
+            type: 'add',
+            payload: newTodo
         });
     }
 
-    const handleDeleteTask = (id) => {
-
-        return () => {
-            const deleteTodo = {
-                id
-            }
-
-            const deleteActionTodo = {
-                type: "delete",
-                payload: deleteTodo
-            }
-
-            dispatch(deleteActionTodo);
-        }
-    }
-
     return (
-        <>
-            <h1>TodoApp <small>({todos.length})</small></h1>
+        <div>
+            <h1>TodoApp ( {todos.length} ) </h1>
             <hr />
 
             <div className="row">
+
                 <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                        {
-                            todos.map((todo, i) => {
-                                return <li key={todo.id} className="list-group-item">
-                                    <p className={`${todo.done && 'complete'}`} onClick={() => handleToogle(todo.id)}>{i + 1} {todo.desc}</p>
-                                    <button className="btn btn-danger" onClick={handleDeleteTask(todo.id)}>Borrar</button>
-                                </li>
-                            })
-                        }
-                    </ul>
+                    <TodoList todos={todos} handleDelete={handleDeleteTask} handleToggle={handleToggle} />
                 </div>
+
                 <div className="col-5">
-                    <h4>Add todo</h4>
-                    <hr />
-
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            name="desc"
-                            className="form-control"
-                            placeholder="Aprender..."
-                            autoComplete="off"
-                            value={desc}
-                            onChange={handleOnChange}
-                        />
-
-                        <button className="btn btn-outline-primary mt-1 btn-block">add</button>
-                    </form>
+                    <TodoAdd handleAddTodo={handleAddTodo} />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
--- src/components/06-useReducer/todoReducer.js
+++ src/components/06-useReducer/todoReducer.js
@@ -1,15 +1,24 @@
+export const todoReducer = ( state = [], action ) => {
 
-export const todoReducer = (state = [], action) => {
+    switch ( action.type ) {
+        case 'add':
+            return [ ...state, action.payload ];
 
-    switch (action.type) {
-        case "add":
-            return [...state, action.payload]
-        case "delete":
-            return state.filter(todo => todo.id !== action.payload.id);
-        case "toogle":
-            return state.map(todo => {
+        case 'delete':
+            return state.filter( todo => todo.id !== action.payload ); // 123123123
 
-                if (todo.id === action.payload) {
+
+        case 'toggle': 
+            return state.map( todo => 
+                ( todo.id === action.payload )
+                    ? { ...todo, done: !todo.done }
+                    : todo
+            );
+
+        case 'toggle-old':
+            return state.map( todo => {
+
+                if ( todo.id === action.payload ) {
                     return {
                         ...todo,
                         done: !todo.done
@@ -17,15 +26,12 @@ export const todoReducer = (state = [], action) => {
                 } else {
                     return todo;
                 }
+
             })
-/*         case "toggle":
-            return state.map(todo =>
-                (todo.id === action.payload)
-                    ? { ...todo, done: !todo.done }
-                    : todo
-            ); */
+
         default:
             return state;
-
     }
+
+
 }
